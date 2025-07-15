4ea68dc72d6763f84d17a7e0bdc1d6e7ed83df9d
pdev <pchiavarino89@gmail.com>
2025-07-14 22:12:09 -0300

    chore: add user and product types definition

---
+++ src/types/index.tsx
@@ -0,0 +1,17 @@
+export type User = {
+  id: number
+  name: string
+  email: string
+  lastname: string
+  phone: string
+  address: string
+  createdAt: string
+}
+
+export type Product = {
+  id: number
+  name: string
+  category: string
+  price: number
+  stock: number
+}
