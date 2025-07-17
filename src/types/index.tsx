1c0ffbd26a7ea1c2edcd5a27841d87fbd0d5e9d4
pdev <pchiavarino89@gmail.com>
2025-07-16 21:50:42 -0300

    chore: add Field type

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,17 +1,25 @@
 export type User = {
-  id: number
-  name: string
-  email: string
-  lastname: string
-  phone: string
-  address: string
-  createdAt: string
+  id: number;
+  name: string;
+  email: string;
+  lastname: string;
+  phone: string;
+  address: string;
+  createdAt: string;
 }
 
 export type Product = {
-  id: number
-  name: string
-  category: string
-  price: number
-  stock: number
+  id: number;
+  name: string;
+  category: string;
+  price: number;
+  stock: number;
+}
+
+export type Field = {
+  title: string;
+  field: string;
+  label: string;
+  type?: 'text' | 'number' | 'boolean' | 'password' | 'email';
+  required?: boolean;
 }
